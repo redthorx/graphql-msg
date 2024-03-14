af697c5ffd8a9f2cfaa0f9641ded0946acd8ebc4
yj <spritofire@gmail.com>
2024-03-14 15:47:48 +0000

    encryption

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import { verify } from 'jsonwebtoken'
 import { Context } from './context'
 
-export const APP_SECRET = '123'
+export const APP_SECRET = process.env.APP_SECRET || '123'
 
 interface Token {
   userId: string
